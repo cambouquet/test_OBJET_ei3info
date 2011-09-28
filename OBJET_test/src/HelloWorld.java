4d824a298859c7e60e80d16ce92648d95e3cbcaa
Camille Bouquet <bouquetcamille@hotmail.fr>
2011-09-28 10:36:52 +0200

    Modification pour vérifier le push avec eclipse

--- OBJET_test/src/HelloWorld.java
+++ OBJET_test/src/HelloWorld.java
@@ -14,6 +14,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Point p1 = new Point();
 		Point p2 = new Point(1, 5);
+		System.out.println("test");
 		
 		System.out.println(p1.toString());
 		System.out.println(p2.toString());
