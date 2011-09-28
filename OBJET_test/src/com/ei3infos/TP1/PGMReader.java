9983c1ff53445e30d25cddf8f9c5ebada2e84d70
Camille Bouquet <bouquetcamille@hotmail.fr>
2011-09-28 11:21:45 +0200

    Créer une classe pour lire un fichier PGM.

---
+++ OBJET_test/src/com/ei3infos/TP1/PGMReader.java
@@ -0,0 +1,26 @@
+package com.ei3infos.TP1;
+
+import java.io.File;
+
+/**
+ * @author Camille
+ *
+ */
+public class PGMReader 
+{
+	private File fichierImage;
+	
+	public PGMReader(File file)
+	{
+		this.fichierImage = file;
+	}
+	
+	public ImagePGM lireFichier()
+	{
+		ImagePGM image;
+		
+		
+		
+		return null;
+	}
+}
